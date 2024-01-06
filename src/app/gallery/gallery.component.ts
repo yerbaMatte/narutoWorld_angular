730b928ef73177c47f845dc82cd169acc6dfc73f
Milosz Lewandowski <113758913+yerbaMatte@users.noreply.github.com>
2024-01-06 13:47:27 +0100

    remove isLoading state

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,24 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { NarutoCharacter } from '../naruto';
 import { CharactersService } from '../core/services/characters.service';
-import { Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, finalize, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.sass'],
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent {
   readonly characters$: Observable<NarutoCharacter[]> = this.charactersService
     .getCharacters(1, 1431)
     .pipe(map((result) => result.characters));
 
-  isLoading = false;
-
   constructor(private charactersService: CharactersService) {}
-
-  ngOnInit(): void {
-    this.characters$.subscribe(console.log);
-    this.isLoading = true;
-  }
 }
